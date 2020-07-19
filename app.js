3fcbf9c4d3082dad3ad001e10f24a7dd92880328
D-Know <n.dino90@gmail.com>
2020-07-19 10:42:21 -0700

    got all of the html populating.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const inquirer = require('inquirer');
 
+const fs = require('fs');
+
+
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -134,6 +138,13 @@ if (!portfolioData.projects) {
   promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
+  
   });
 
